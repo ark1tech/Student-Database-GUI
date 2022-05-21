4a5c2d3af8de4a4fe240da0f5e26baf5ce382f3d
Arki <rbmanago1@up.edu.ph>
2022-05-21 13:33:02 +0800

    meow

--- src/MP/StudentDBDemo.java
+++ src/MP/StudentDBDemo.java
@@ -170,54 +170,54 @@ public class StudentDBDemo {
 		scrollPane.setPreferredSize(new Dimension(702, 575));
 		scrollPane.setBounds(195, 6, 702, 575);
 		bottomCard.add(scrollPane);
-		
-				JPanel viewCard = new JPanel();
-				displayPanel.add(viewCard, "view");
-				viewCard.setLayout(null);
-				
-						JPanel entryPanel_1 = new JPanel();
-						entryPanel_1.setBackground(new Color(230, 230, 250));
-						entryPanel_1.setBounds(6, 6, 672, 142);
-						viewCard.add(entryPanel_1);
-						entryPanel_1.setLayout(null);
-						
-								JLabel nameLabel_1 = new JLabel("Raymund Klien B. Mañago");
-								nameLabel_1.setFont(new Font("Arial", Font.PLAIN, 25));
-								nameLabel_1.setBounds(31, 18, 455, 48);
-								entryPanel_1.add(nameLabel_1);
-								
-										JLabel saisLabel_1 = new JLabel("SAIS ID:");
-										saisLabel_1.setForeground(Color.DARK_GRAY);
-										saisLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
-										saisLabel_1.setBounds(31, 69, 57, 24);
-										entryPanel_1.add(saisLabel_1);
-										
-												JLabel studNoLabel_1 = new JLabel("STUDENT NUMBER:");
-												studNoLabel_1.setForeground(Color.DARK_GRAY);
-												studNoLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
-												studNoLabel_1.setBounds(172, 69, 150, 24);
-												entryPanel_1.add(studNoLabel_1);
-												
-														JLabel saisInfo_1 = new JLabel("235412");
-														saisInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
-														saisInfo_1.setBounds(97, 69, 63, 24);
-														entryPanel_1.add(saisInfo_1);
-														
-																JLabel studNoInfo_1 = new JLabel("201054321");
-																studNoInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
-																studNoInfo_1.setBounds(326, 69, 105, 24);
-																entryPanel_1.add(studNoInfo_1);
-																
-																		JLabel addressLabel_1 = new JLabel("ADDRESS:");
-																		addressLabel_1.setForeground(Color.DARK_GRAY);
-																		addressLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
-																		addressLabel_1.setBounds(31, 90, 91, 24);
-																		entryPanel_1.add(addressLabel_1);
-																		
-																				JLabel addressInfo_1 = new JLabel("Ili Sur, San Juan, 2514, La Union");
-																				addressInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
-																				addressInfo_1.setBounds(117, 90, 345, 24);
-																				entryPanel_1.add(addressInfo_1);
+
+		JPanel viewCard = new JPanel();
+		displayPanel.add(viewCard, "view");
+		viewCard.setLayout(null);
+
+		JPanel entryPanel_1 = new JPanel();
+		entryPanel_1.setBackground(new Color(230, 230, 250));
+		entryPanel_1.setBounds(6, 6, 672, 142);
+		viewCard.add(entryPanel_1);
+		entryPanel_1.setLayout(null);
+
+		JLabel nameLabel_1 = new JLabel("Raymund Klien B. Mañago");
+		nameLabel_1.setFont(new Font("Arial", Font.PLAIN, 25));
+		nameLabel_1.setBounds(31, 18, 455, 48);
+		entryPanel_1.add(nameLabel_1);
+
+		JLabel saisLabel_1 = new JLabel("SAIS ID:");
+		saisLabel_1.setForeground(Color.DARK_GRAY);
+		saisLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		saisLabel_1.setBounds(31, 69, 57, 24);
+		entryPanel_1.add(saisLabel_1);
+
+		JLabel studNoLabel_1 = new JLabel("STUDENT NUMBER:");
+		studNoLabel_1.setForeground(Color.DARK_GRAY);
+		studNoLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		studNoLabel_1.setBounds(172, 69, 150, 24);
+		entryPanel_1.add(studNoLabel_1);
+
+		JLabel saisInfo_1 = new JLabel("235412");
+		saisInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		saisInfo_1.setBounds(97, 69, 63, 24);
+		entryPanel_1.add(saisInfo_1);
+
+		JLabel studNoInfo_1 = new JLabel("201054321");
+		studNoInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		studNoInfo_1.setBounds(326, 69, 105, 24);
+		entryPanel_1.add(studNoInfo_1);
+
+		JLabel addressLabel_1 = new JLabel("ADDRESS:");
+		addressLabel_1.setForeground(Color.DARK_GRAY);
+		addressLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		addressLabel_1.setBounds(31, 90, 91, 24);
+		entryPanel_1.add(addressLabel_1);
+
+		JLabel addressInfo_1 = new JLabel("Ili Sur, San Juan, 2514, La Union");
+		addressInfo_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		addressInfo_1.setBounds(117, 90, 345, 24);
+		entryPanel_1.add(addressInfo_1);
 
 		JPanel editCard = new JPanel();
 		displayPanel.add(editCard, "edit");
@@ -310,7 +310,7 @@ public class StudentDBDemo {
 		addressAddLabel.setFont(new Font("Arial", Font.PLAIN, 15));
 		addressAddLabel.setBounds(57, 118, 112, 24);
 		addPanel.add(addressAddLabel);
-		
+
 		nameAddField = new JTextField();
 		nameAddField.setBounds(182, 48, 426, 26);
 		nameAddField.setColumns(10);
@@ -337,21 +337,21 @@ public class StudentDBDemo {
 		addInstruc.setFont(new Font("Arial", Font.PLAIN, 15));
 		addInstruc.setBounds(17, 13, 230, 24);
 		addPanel.add(addInstruc);
-		
+
 		JLabel addFailLabel = new JLabel("");
 		addFailLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		addFailLabel.setForeground(Color.RED);
 		addFailLabel.setFont(new Font("Arial", Font.PLAIN, 15));
 		addFailLabel.setBounds(184, 146, 118, 24);
 		addPanel.add(addFailLabel);
-		
+
 		JLabel addSuccessLabel = new JLabel("");
 		addSuccessLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		addSuccessLabel.setForeground(new Color (0x8c52ff));
 		addSuccessLabel.setFont(new Font("Arial", Font.PLAIN, 15));
 		addSuccessLabel.setBounds(205, 403, 302, 24);
 		addCard.add(addSuccessLabel);
-		
+
 		JButton addEntryBtn = new JButton("Add");
 		addEntryBtn.setBounds(549, 172, 117, 29);
 		addEntryBtn.addActionListener(new ActionListener() {
@@ -367,7 +367,7 @@ public class StudentDBDemo {
 			}
 		});
 		addPanel.add(addEntryBtn);
-		
+
 		JButton btnReset = new JButton("Reset");
 		btnReset.setBounds(420, 172, 117, 29);
 		btnReset.addActionListener(new ActionListener() {
@@ -590,7 +590,7 @@ public class StudentDBDemo {
 		});
 
 		viewButton.addActionListener(e -> cl2.show(displayPanel, "view"));
-		
+
 
 		JLabel menuLabel = new JLabel("<html> <b> Menu </b> </html>");
 		menuLabel.setBounds(8, 5, 180, 50);
