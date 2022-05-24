e8221a5acaa83774f4e0a2e6567cf39550bfbd09
Marilag <offcllance@gmail.com>
2022-05-24 22:25:44 +0800

    Update StudentData.java
    StudentData is now a proper class with setters and getters. Thank you, Eclipse for doing it automatically.

--- src/MP/StudentData.java
+++ src/MP/StudentData.java
@@ -1,5 +1,53 @@
 package MP;
 
+import java.io.*;
+import java.util.*;
+
 public class StudentData {
+	
+	String name, address;
+	int id, num;
+	
+	static int n = 0;
+	
+	public StudentData(String name, int id, int num, String address) {
+		this.name = name;
+		this.id = id;
+		this.num = num;
+		this.address = address;
+		n++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
 
 }
